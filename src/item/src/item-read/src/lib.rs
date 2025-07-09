a85c28f94bebc0260f74bc9848b3ed6c4464d0f7
bruderj15 <julian.bruder@outlook.com>
2025-07-09 22:37:39 +0200

    main: added crate `item-read`

---
+++ src/item/src/item-read/src/lib.rs
@@ -0,0 +1,94 @@
+use async_trait::async_trait;
+use aws_sdk_dynamodb::config::http::HttpResponse;
+use aws_sdk_dynamodb::error::SdkError;
+use aws_sdk_dynamodb::operation::get_item::GetItemError;
+use aws_sdk_dynamodb::operation::query::QueryError;
+use aws_sdk_dynamodb::types::AttributeValue;
+use common::has::Has;
+use common::shop_id::ShopId;
+use common::shops_item_id::ShopsItemId;
+use item_core::item::diff_record::ItemDiffRecord;
+use item_core::item::record::ItemRecord;
+use tracing::error;
+
+#[async_trait]
+pub trait ReadItemRecords {
+    async fn get_item_record(
+        &self,
+        shop_id: &ShopId,
+        shops_item_id: &ShopsItemId,
+    ) -> Result<Option<ItemRecord>, SdkError<GetItemError, HttpResponse>>;
+
+    async fn query_item_diff_records(
+        &self,
+        shop_id: &ShopId,
+        scan_index_forward: bool,
+    ) -> Result<Vec<ItemDiffRecord>, SdkError<QueryError, HttpResponse>>;
+}
+
+#[async_trait]
+impl<T> ReadItemRecords for T
+where
+    T: Has<aws_sdk_dynamodb::Client> + Sync,
+{
+    async fn get_item_record(
+        &self,
+        shop_id: &ShopId,
+        shops_item_id: &ShopsItemId,
+    ) -> Result<Option<ItemRecord>, SdkError<GetItemError, HttpResponse>> {
+        let pk = format!("item#shop_id#{shop_id}#shops_item_id#{shops_item_id}");
+        let sk = "item#materialized".to_string();
+        let rec = self
+            .get()
+            .get_item()
+            .table_name("items")
+            .key("pk", AttributeValue::S(pk))
+            .key("sk", AttributeValue::S(sk))
+            .send()
+            .await?
+            .item
+            .map(serde_dynamo::from_item::<_, ItemRecord>)
+            .and_then(|item_record_res| match item_record_res {
+                Ok(item_record) => Some(item_record),
+                Err(err) => {
+                    error!(error = %err, "Failed deserializing PrescriptionEventRecord.");
+                    None
+                }
+            });
+
+        Ok(rec)
+    }
+
+    async fn query_item_diff_records(
+        &self,
+        shop_id: &ShopId,
+        scan_index_forward: bool,
+    ) -> Result<Vec<ItemDiffRecord>, SdkError<QueryError, HttpResponse>> {
+        let records = self
+            .get()
+            .query()
+            .table_name("items")
+            .index_name("gsi_1")
+            .key_condition_expression("#gsi_1_pk = :gsi_1_pk_val")
+            .expression_attribute_names("#gsi_1_pk", "gsi_1_pk")
+            .expression_attribute_values(":gsi_1_pk_val", AttributeValue::S(shop_id.to_string()))
+            .scan_index_forward(scan_index_forward)
+            .into_paginator()
+            .send()
+            .try_collect()
+            .await?
+            .into_iter()
+            .flat_map(|qo| qo.items.unwrap_or_default())
+            .map(serde_dynamo::from_item::<_, ItemDiffRecord>)
+            .filter_map(|result| match result {
+                Ok(event) => Some(event),
+                Err(err) => {
+                    error!(error = %err, "Failed deserializing ItemDiffRecord.");
+                    None
+                }
+            })
+            .collect();
+
+        Ok(records)
+    }
+}
---
+++ src/item/src/item-read/tests/lib.rs
@@ -0,0 +1,405 @@
+use common::currency::record::CurrencyRecord;
+use common::event_id::EventId;
+use common::item_id::ItemId;
+use common::shop_id::ShopId;
+use common::shops_item_id::ShopsItemId;
+use item_core::item::diff_record::ItemDiffRecord;
+use item_core::item::record::ItemRecord;
+use item_core::item_state::record::ItemStateRecord;
+use item_read::ReadItemRecords;
+use std::time::Duration;
+use test_api::tokio::time::sleep;
+use test_api::*;
+use time::OffsetDateTime;
+use time::format_description::well_known;
+
+#[localstack_test(services = [DynamoDB])]
+async fn should_return_nothing_for_get_item_record_when_table_is_empty() {
+    let client = get_dynamodb_client().await;
+    let actual = client
+        .get_item_record(&ShopId::new(), &"non-existent".into())
+        .await
+        .unwrap();
+
+    assert!(actual.is_none());
+}
+
+#[localstack_test(services = [DynamoDB])]
+async fn should_return_item_record_for_get_item_record_when_exists() {
+    let now = OffsetDateTime::now_utc();
+    let now_str = now.format(&well_known::Rfc3339).unwrap();
+    let shop_id = ShopId::new();
+    let shops_item_id: ShopsItemId = "123465".into();
+    let expected = ItemRecord {
+        pk: format!("item#shop_id#{shop_id}#shops_item_id#{shops_item_id}"),
+        sk: "item#materialized".to_string(),
+        gsi_1_pk: shop_id.into(),
+        gsi_1_sk: now_str.clone(),
+        item_id: ItemId::now(),
+        event_id: EventId::new(),
+        shop_id,
+        shops_item_id: shops_item_id.clone(),
+        shop_name: "Foo".to_string(),
+        title_de: Some("Bar".to_string()),
+        title_en: Some("Barr".to_string()),
+        description_de: Some("Baz".to_string()),
+        description_en: Some("Bazz".to_string()),
+        price_currency: CurrencyRecord::Eur,
+        price_amount: 110.5,
+        state: ItemStateRecord::Available,
+        url: "https:://foo.bar/123456".to_string(),
+        images: vec!["https:://foo.bar/123456/image".to_string()],
+        hash: "123456".to_string(),
+        created: now,
+        updated: now,
+    };
+
+    let client = get_dynamodb_client().await;
+    client
+        .put_item()
+        .table_name("items")
+        .set_item(serde_dynamo::to_item(&expected).ok())
+        .send()
+        .await
+        .unwrap();
+
+    let actual = client
+        .get_item_record(&shop_id, &shops_item_id)
+        .await
+        .unwrap();
+
+    assert!(actual.is_some());
+    assert_eq!(expected, actual.unwrap());
+}
+
+#[localstack_test(services = [DynamoDB])]
+async fn should_return_nothing_for_get_item_record_when_only_others_exist() {
+    let now = OffsetDateTime::now_utc();
+    let now_str = now.format(&well_known::Rfc3339).unwrap();
+    let shop_id = ShopId::new();
+    let shops_item_id: ShopsItemId = "123465".into();
+    let expected = ItemRecord {
+        pk: format!("item#shop_id#{shop_id}#shops_item_id#{shops_item_id}"),
+        sk: "item#materialized".to_string(),
+        gsi_1_pk: shop_id.into(),
+        gsi_1_sk: now_str.clone(),
+        item_id: ItemId::now(),
+        event_id: EventId::new(),
+        shop_id,
+        shops_item_id: shops_item_id.clone(),
+        shop_name: "Foo".to_string(),
+        title_de: Some("Bar".to_string()),
+        title_en: Some("Barr".to_string()),
+        description_de: Some("Baz".to_string()),
+        description_en: Some("Bazz".to_string()),
+        price_currency: CurrencyRecord::Eur,
+        price_amount: 110.5,
+        state: ItemStateRecord::Available,
+        url: "https:://foo.bar/123456".to_string(),
+        images: vec!["https:://foo.bar/123456/image".to_string()],
+        hash: "123456".to_string(),
+        created: now,
+        updated: now,
+    };
+
+    let client = get_dynamodb_client().await;
+    client
+        .put_item()
+        .table_name("items")
+        .set_item(serde_dynamo::to_item(&expected).ok())
+        .send()
+        .await
+        .unwrap();
+
+    let actual = client
+        .get_item_record(&ShopId::new(), &"non-existent".into())
+        .await
+        .unwrap();
+
+    assert!(actual.is_none());
+}
+
+#[localstack_test(services = [DynamoDB])]
+async fn should_return_nothing_for_query_item_diff_records_when_table_is_empty() {
+    let client = get_dynamodb_client().await;
+    let actual = client
+        .query_item_diff_records(&ShopId::new(), true)
+        .await
+        .unwrap();
+
+    assert!(actual.is_empty());
+}
+
+#[localstack_test(services = [DynamoDB])]
+async fn should_return_item_diff_record_for_query_item_diff_records_when_exists() {
+    let now = OffsetDateTime::now_utc();
+    let now_str = now.format(&well_known::Rfc3339).unwrap();
+    let shop_id = ShopId::new();
+    let shops_item_id: ShopsItemId = "123465".into();
+    let inserted = ItemRecord {
+        pk: format!("item#shop_id#{shop_id}#shops_item_id#{shops_item_id}"),
+        sk: "item#materialized".to_string(),
+        gsi_1_pk: shop_id.into(),
+        gsi_1_sk: now_str.clone(),
+        item_id: ItemId::now(),
+        event_id: EventId::new(),
+        shop_id,
+        shops_item_id: shops_item_id.clone(),
+        shop_name: "Foo".to_string(),
+        title_de: Some("Bar".to_string()),
+        title_en: Some("Barr".to_string()),
+        description_de: Some("Baz".to_string()),
+        description_en: Some("Bazz".to_string()),
+        price_currency: CurrencyRecord::Eur,
+        price_amount: 110.5,
+        state: ItemStateRecord::Available,
+        url: "https:://foo.bar/123456".to_string(),
+        images: vec!["https:://foo.bar/123456/image".to_string()],
+        hash: "123456".to_string(),
+        created: now,
+        updated: now,
+    };
+
+    let client = get_dynamodb_client().await;
+    client
+        .put_item()
+        .table_name("items")
+        .set_item(serde_dynamo::to_item(&inserted).ok())
+        .send()
+        .await
+        .unwrap();
+
+    // Wait for GSI
+    sleep(Duration::from_secs(3)).await;
+
+    let expected: ItemDiffRecord = inserted.into();
+    let actual = client
+        .query_item_diff_records(&shop_id, true)
+        .await
+        .unwrap();
+
+    assert_eq!(vec![expected], actual);
+}
+
+#[localstack_test(services = [DynamoDB])]
+async fn should_return_item_diff_records_for_query_item_diff_records_when_exists() {
+    let now1 = OffsetDateTime::now_utc();
+    let now1_str = now1.format(&well_known::Rfc3339).unwrap();
+    let shop_id = ShopId::new();
+    let shops_item_id_1: ShopsItemId = "123465".into();
+    let inserted1 = ItemRecord {
+        pk: format!("item#shop_id#{shop_id}#shops_item_id#{shops_item_id_1}"),
+        sk: "item#materialized".to_string(),
+        gsi_1_pk: shop_id.into(),
+        gsi_1_sk: now1_str.clone(),
+        item_id: ItemId::now(),
+        event_id: EventId::new(),
+        shop_id,
+        shops_item_id: shops_item_id_1.clone(),
+        shop_name: "Foo".to_string(),
+        title_de: Some("Bar".to_string()),
+        title_en: Some("Barr".to_string()),
+        description_de: Some("Baz".to_string()),
+        description_en: Some("Bazz".to_string()),
+        price_currency: CurrencyRecord::Eur,
+        price_amount: 110.5,
+        state: ItemStateRecord::Available,
+        url: "https:://foo.bar/123456".to_string(),
+        images: vec!["https:://foo.bar/123456/image".to_string()],
+        hash: "123456".to_string(),
+        created: now1,
+        updated: now1,
+    };
+    let shops_item_id_2: ShopsItemId = "abcdefg".into();
+    let now2 = OffsetDateTime::now_utc();
+    let now2_str = now2.format(&well_known::Rfc3339).unwrap();
+    let inserted2 = ItemRecord {
+        pk: format!("item#shop_id#{shop_id}#shops_item_id#{shops_item_id_2}"),
+        sk: "item#materialized".to_string(),
+        gsi_1_pk: shop_id.into(),
+        gsi_1_sk: now2_str.clone(),
+        item_id: ItemId::now(),
+        event_id: EventId::new(),
+        shop_id,
+        shops_item_id: shops_item_id_2.clone(),
+        shop_name: "Foo".to_string(),
+        title_de: Some("Bar".to_string()),
+        title_en: Some("Barr".to_string()),
+        description_de: Some("Baz".to_string()),
+        description_en: Some("Bazz".to_string()),
+        price_currency: CurrencyRecord::Eur,
+        price_amount: 110.5,
+        state: ItemStateRecord::Available,
+        url: "https:://foo.bar/123456".to_string(),
+        images: vec!["https:://foo.bar/123456/image".to_string()],
+        hash: "123456".to_string(),
+        created: now2,
+        updated: now2,
+    };
+
+    let client = get_dynamodb_client().await;
+    client
+        .put_item()
+        .table_name("items")
+        .set_item(serde_dynamo::to_item(&inserted1).ok())
+        .send()
+        .await
+        .unwrap();
+    client
+        .put_item()
+        .table_name("items")
+        .set_item(serde_dynamo::to_item(&inserted2).ok())
+        .send()
+        .await
+        .unwrap();
+
+    // Wait for GSI
+    sleep(Duration::from_secs(3)).await;
+
+    let expected1: ItemDiffRecord = inserted1.into();
+    let expected2: ItemDiffRecord = inserted2.into();
+    let actual = client
+        .query_item_diff_records(&shop_id, true)
+        .await
+        .unwrap();
+
+    assert_eq!(2, actual.len());
+    assert!(actual.contains(&expected1));
+    assert!(actual.contains(&expected2));
+}
+
+#[localstack_test(services = [DynamoDB])]
+async fn should_return_item_diff_records_sorted_by_created_latest_for_query_item_diff_records_when_exists_and_scan_forward()
+ {
+    let now1 = OffsetDateTime::now_utc();
+    let now1_str = now1.format(&well_known::Rfc3339).unwrap();
+    let shop_id = ShopId::new();
+    let shops_item_id_1: ShopsItemId = "123465".into();
+    let inserted1 = ItemRecord {
+        pk: format!("item#shop_id#{shop_id}#shops_item_id#{shops_item_id_1}"),
+        sk: "item#materialized".to_string(),
+        gsi_1_pk: shop_id.into(),
+        gsi_1_sk: now1_str.clone(),
+        item_id: ItemId::now(),
+        event_id: EventId::new(),
+        shop_id,
+        shops_item_id: shops_item_id_1.clone(),
+        shop_name: "Foo".to_string(),
+        title_de: Some("Bar".to_string()),
+        title_en: Some("Barr".to_string()),
+        description_de: Some("Baz".to_string()),
+        description_en: Some("Bazz".to_string()),
+        price_currency: CurrencyRecord::Eur,
+        price_amount: 110.5,
+        state: ItemStateRecord::Available,
+        url: "https:://foo.bar/123456".to_string(),
+        images: vec!["https:://foo.bar/123456/image".to_string()],
+        hash: "123456".to_string(),
+        created: now1,
+        updated: now1,
+    };
+    let shops_item_id_2: ShopsItemId = "abcdefg".into();
+    let now2 = OffsetDateTime::now_utc();
+    let now2_str = now2.format(&well_known::Rfc3339).unwrap();
+    let inserted2 = ItemRecord {
+        pk: format!("item#shop_id#{shop_id}#shops_item_id#{shops_item_id_2}"),
+        sk: "item#materialized".to_string(),
+        gsi_1_pk: shop_id.into(),
+        gsi_1_sk: now2_str.clone(),
+        item_id: ItemId::now(),
+        event_id: EventId::new(),
+        shop_id,
+        shops_item_id: shops_item_id_2.clone(),
+        shop_name: "Foo".to_string(),
+        title_de: Some("Bar".to_string()),
+        title_en: Some("Barr".to_string()),
+        description_de: Some("Baz".to_string()),
+        description_en: Some("Bazz".to_string()),
+        price_currency: CurrencyRecord::Eur,
+        price_amount: 110.5,
+        state: ItemStateRecord::Available,
+        url: "https:://foo.bar/123456".to_string(),
+        images: vec!["https:://foo.bar/123456/image".to_string()],
+        hash: "123456".to_string(),
+        created: now2,
+        updated: now2,
+    };
+
+    let client = get_dynamodb_client().await;
+    client
+        .put_item()
+        .table_name("items")
+        .set_item(serde_dynamo::to_item(&inserted1).ok())
+        .send()
+        .await
+        .unwrap();
+    client
+        .put_item()
+        .table_name("items")
+        .set_item(serde_dynamo::to_item(&inserted2).ok())
+        .send()
+        .await
+        .unwrap();
+
+    // Wait for GSI
+    sleep(Duration::from_secs(3)).await;
+
+    let expected1: ItemDiffRecord = inserted1.into();
+    let expected2: ItemDiffRecord = inserted2.into();
+    let actual = client
+        .query_item_diff_records(&shop_id, true)
+        .await
+        .unwrap();
+
+    assert_eq!(vec![expected1, expected2], actual);
+}
+
+#[localstack_test(services = [DynamoDB])]
+async fn should_return_nothing_for_query_item_diff_records_when_only_others_exist() {
+    let now = OffsetDateTime::now_utc();
+    let now_str = now.format(&well_known::Rfc3339).unwrap();
+    let shop_id = ShopId::new();
+    let shops_item_id: ShopsItemId = "123465".into();
+    let inserted = ItemRecord {
+        pk: format!("item#shop_id#{shop_id}#shops_item_id#{shops_item_id}"),
+        sk: "item#materialized".to_string(),
+        gsi_1_pk: shop_id.into(),
+        gsi_1_sk: now_str.clone(),
+        item_id: ItemId::now(),
+        event_id: EventId::new(),
+        shop_id,
+        shops_item_id: shops_item_id.clone(),
+        shop_name: "Foo".to_string(),
+        title_de: Some("Bar".to_string()),
+        title_en: Some("Barr".to_string()),
+        description_de: Some("Baz".to_string()),
+        description_en: Some("Bazz".to_string()),
+        price_currency: CurrencyRecord::Eur,
+        price_amount: 110.5,
+        state: ItemStateRecord::Available,
+        url: "https:://foo.bar/123456".to_string(),
+        images: vec!["https:://foo.bar/123456/image".to_string()],
+        hash: "123456".to_string(),
+        created: now,
+        updated: now,
+    };
+
+    let client = get_dynamodb_client().await;
+    client
+        .put_item()
+        .table_name("items")
+        .set_item(serde_dynamo::to_item(&inserted).ok())
+        .send()
+        .await
+        .unwrap();
+
+    // Wait for GSI
+    sleep(Duration::from_secs(3)).await;
+
+    let actual = client
+        .query_item_diff_records(&ShopId::new(), true)
+        .await
+        .unwrap();
+
+    assert!(actual.is_empty());
+}
