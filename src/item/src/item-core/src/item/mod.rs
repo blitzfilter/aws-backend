c840b1a1fad1b6594c91dba4a45d568790646148
bruderj15 <julian.bruder@outlook.com>
2025-07-10 17:49:29 +0200

    main: specify write-item trait

--- src/item/src/item-core/src/item/mod.rs
+++ src/item/src/item-core/src/item/mod.rs
@@ -2,3 +2,4 @@ pub mod diff_record;
 pub mod document;
 pub mod document_update;
 pub mod record;
+pub mod update_record;
--- src/item/src/item-write/src/lib.rs
+++ src/item/src/item-write/src/lib.rs
@@ -2,23 +2,31 @@ use async_trait::async_trait;
 use aws_sdk_dynamodb::config::http::HttpResponse;
 use aws_sdk_dynamodb::error::SdkError;
 use aws_sdk_dynamodb::operation::batch_write_item::{BatchWriteItemError, BatchWriteItemOutput};
+use aws_sdk_dynamodb::operation::update_item::{UpdateItemError, UpdateItemOutput};
 use common::dynamodb_batch::DynamoDbBatch;
+use common::shop_id::ShopId;
+use common::shops_item_id::ShopsItemId;
 use item_core::item::record::ItemRecord;
+use item_core::item::update_record::ItemUpdateRecord;
 use item_core::item_event::record::ItemEventRecord;
 
 #[async_trait]
 #[allow(clippy::result_large_err)]
 pub trait WriteItemRecords {
-    fn append_item_event_records(
+    fn put_item_event_records(
         &self,
-        event_records: DynamoDbBatch<ItemEventRecord>,
+        item_event_records: DynamoDbBatch<ItemEventRecord>,
     ) -> Result<BatchWriteItemOutput, SdkError<BatchWriteItemError, HttpResponse>>;
 
-    fn write_item_records(
+    fn put_item_records(
         &self,
-        event_records: DynamoDbBatch<ItemRecord>,
+        item_records: DynamoDbBatch<ItemRecord>,
     ) -> Result<BatchWriteItemOutput, SdkError<BatchWriteItemError, HttpResponse>>;
 
-    // TODO: Patch-Type for item-record-updates!
-    //       Maybe with a clever From<ItemRecord>...?
+    fn update_item_record(
+        &self,
+        shop_id: &ShopId,
+        shops_item_id: &ShopsItemId,
+        event_records: ItemUpdateRecord,
+    ) -> Result<UpdateItemOutput, SdkError<UpdateItemError, HttpResponse>>;
 }
