12e112e7c3a35de999d20e7b2f475bd34601f4cc
bruderj15 <julian.bruder@outlook.com>
2025-07-13 12:07:04 +0200

    main: add trait `IndexItemDocuments`

--- src/item/src/item-index/src/lib.rs
+++ src/item/src/item-index/src/lib.rs
@@ -2,6 +2,68 @@ pub mod bulk;
 
 use crate::bulk::BulkResponse;
 use async_trait::async_trait;
+use common::has::Has;
+use common::item_id::ItemId;
+use item_core::item::document::ItemDocument;
+use item_core::item::document_update::ItemDocumentUpdate;
+use opensearch::{BulkOperation, BulkOperations, BulkParts};
+use serde_json::json;
+use std::collections::HashMap;
 
 #[async_trait]
-pub trait IndexItemDocuments {}
+pub trait IndexItemDocuments {
+    async fn create_item_documents(
+        &self,
+        updates: Vec<ItemDocument>,
+    ) -> Result<BulkResponse, opensearch::Error>;
+
+    async fn update_item_documents(
+        &self,
+        updates: HashMap<ItemId, ItemDocumentUpdate>,
+    ) -> Result<BulkResponse, opensearch::Error>;
+}
+
+#[async_trait]
+impl<T: Has<opensearch::OpenSearch> + Sync> IndexItemDocuments for T {
+    async fn create_item_documents(
+        &self,
+        documents: Vec<ItemDocument>,
+    ) -> Result<BulkResponse, opensearch::Error> {
+        let mut ops = BulkOperations::new();
+
+        for doc in documents {
+            ops.push(BulkOperation::create(doc._id(), &doc))?;
+        }
+
+        self.get()
+            .bulk(BulkParts::Index("items"))
+            .body(vec![ops])
+            .send()
+            .await?
+            .json::<BulkResponse>()
+            .await
+    }
+
+    async fn update_item_documents(
+        &self,
+        updates: HashMap<ItemId, ItemDocumentUpdate>,
+    ) -> Result<BulkResponse, opensearch::Error> {
+        let mut ops = BulkOperations::new();
+        for (_id, doc) in updates {
+            ops.push(BulkOperation::update(
+                _id,
+                json!({
+                "doc": doc
+                }),
+            ))?;
+        }
+
+        self.get()
+            .bulk(BulkParts::Index("items"))
+            .body(vec![ops])
+            .send()
+            .await?
+            .json::<BulkResponse>()
+            .await
+    }
+}
