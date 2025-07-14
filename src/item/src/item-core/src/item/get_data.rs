e2c81bfbfac2f6cb18963142d3c0acaf5d3001c8
bruderj15 <julian.bruder@outlook.com>
2025-07-14 21:03:32 +0200

    main: add get-item dto

---
+++ src/item/src/item-core/src/item/get_data.rs
@@ -0,0 +1,45 @@
+use crate::item_state::data::ItemStateData;
+use common::event_id::EventId;
+use common::item_id::ItemId;
+use common::price::data::PriceData;
+use common::shop_id::ShopId;
+use common::shops_item_id::ShopsItemId;
+use serde::Serialize;
+use time::OffsetDateTime;
+
+#[derive(Debug, Clone, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetItemData {
+    pub item_id: ItemId,
+
+    pub event_id: EventId,
+
+    pub shop_id: ShopId,
+
+    pub shops_item_id: ShopsItemId,
+
+    pub shop_name: String,
+
+    pub title: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub price: Option<PriceData>,
+
+    pub state: ItemStateData,
+
+    pub url: String,
+
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub images: Vec<String>,
+
+    pub hash: String,
+
+    #[serde(with = "time::serde::rfc3339")]
+    pub created: OffsetDateTime,
+
+    #[serde(with = "time::serde::rfc3339")]
+    pub updated: OffsetDateTime,
+}
--- src/item/src/item-core/src/item/mod.rs
+++ src/item/src/item-core/src/item/mod.rs
@@ -1,5 +1,6 @@
 pub mod diff_record;
 pub mod document;
 pub mod document_update;
+pub mod get_data;
 pub mod record;
 pub mod update_record;
