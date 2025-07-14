a754525285fb76c933e62df511c326295adcaaaa
bruderj15 <julian.bruder@outlook.com>
2025-07-14 20:37:48 +0200

    main: add item-state dto

--- src/common/src/lib.rs
+++ src/common/src/lib.rs
@@ -6,6 +6,7 @@ pub mod event_id;
 pub mod has;
 pub mod item_id;
 pub mod language;
+pub mod price;
 pub mod serde;
 pub mod shop_id;
 pub mod shops_item_id;
---
+++ src/item/src/item-core/src/item_state/data.rs
@@ -0,0 +1,48 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ItemStateData {
+    Listed,
+    Available,
+    Reserved,
+    Sold,
+    Removed,
+    Unknown,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ItemStateData;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(ItemStateData::Listed, "\"LISTED\"")]
+    #[case(ItemStateData::Available, "\"AVAILABLE\"")]
+    #[case(ItemStateData::Reserved, "\"RESERVED\"")]
+    #[case(ItemStateData::Sold, "\"SOLD\"")]
+    #[case(ItemStateData::Removed, "\"REMOVED\"")]
+    #[case(ItemStateData::Unknown, "\"UNKNOWN\"")]
+    fn should_serialize_item_state_data_in_screaming_snake_case(
+        #[case] item_state_record: ItemStateData,
+        #[case] expected: &str,
+    ) {
+        let actual = serde_json::to_string(&item_state_record).unwrap();
+        assert_eq!(actual, expected);
+    }
+
+    #[rstest]
+    #[case("\"LISTED\"", ItemStateData::Listed)]
+    #[case("\"AVAILABLE\"", ItemStateData::Available)]
+    #[case("\"RESERVED\"", ItemStateData::Reserved)]
+    #[case("\"SOLD\"", ItemStateData::Sold)]
+    #[case("\"REMOVED\"", ItemStateData::Removed)]
+    #[case("\"UNKNOWN\"", ItemStateData::Unknown)]
+    fn should_deserialize_item_state_data_in_screaming_snake_case(
+        #[case] currency: &str,
+        #[case] expected: ItemStateData,
+    ) {
+        let actual = serde_json::from_str::<ItemStateData>(currency).unwrap();
+        assert_eq!(actual, expected);
+    }
+}
