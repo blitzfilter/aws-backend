9621f7a88723dcb0f4e382450e53de3475eed255
bruderj15 <julian.bruder@outlook.com>
2025-07-21 19:23:03 +0200

    main: add `ShopsItemId::new` with random uuid

--- src/common/src/shops_item_id.rs
+++ src/common/src/shops_item_id.rs
@@ -1,9 +1,22 @@
 use serde::{Deserialize, Serialize};
 use std::fmt::{Display, Formatter};
+use uuid::Uuid;
 
 #[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Hash, Serialize, Deserialize)]
 #[serde(transparent)]
 pub struct ShopsItemId(String);
+
+impl ShopsItemId {
+    pub fn new() -> Self {
+        Self(Uuid::new_v4().to_string())
+    }
+}
+
+impl Default for ShopsItemId {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
 impl Display for ShopsItemId {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
