7d0d58bf6892fdd25c5e58f4c4c6d2257e8b2a64
bruderj15 <julian.bruder@outlook.com>
2025-07-21 20:23:16 +0200

    main: test many updates for `InboundWriteItem`

--- src/item/src/item-write/tests/service.rs
+++ src/item/src/item-write/tests/service.rs
@@ -1,13 +1,20 @@
 use common::aggregate::Aggregate;
+use common::batch::Batch;
+use common::item_id::ItemKey;
 use common::shop_id::ShopId;
 use common::shops_item_id::ShopsItemId;
-use item_core::item::command::CreateItemCommand;
+use item_core::item::command::{CreateItemCommand, UpdateItemCommand};
 use item_core::item::domain::Item;
+use item_core::item::hash::ItemHash;
+use item_core::item::record::ItemRecord;
 use item_core::item_event::domain::ItemEvent;
 use item_core::item_event::record::ItemEventRecord;
 use item_core::item_state::domain::ItemState;
+use item_core::item_state::record::ItemStateRecord;
+use item_write::repository::WriteItemRecords;
 use item_write::service::InboundWriteItems;
 use test_api::*;
+use time::OffsetDateTime;
 
 #[localstack_test(services = [DynamoDB])]
 async fn should_create_items_for_handle_create_items_with_one_command() {
@@ -91,4 +98,136 @@ async fn should_create_items_for_handle_create_items_with_commands_count(#[case]
         .count;
 
     assert_eq!(count, actual_count);
+}
+
+#[rstest::rstest]
+#[case::ten(10)]
+#[case::twentyfive(25)]
+#[case::fortytwo(42)]
+#[case::fifty(50)]
+#[case::sixtynine(69)]
+#[case::onehundred(100)]
+#[case::fourhundredandtwenty(420)]
+#[case::fivehundred(500)]
+#[localstack_test(services = [DynamoDB])]
+async fn should_fail_non_existent_items_for_handle_update_items_with_commands_count(
+    #[case] count: i32,
+) {
+    let shop_id = ShopId::new();
+    let mk_entry = |x: i32| {
+        (
+            ItemKey::new(shop_id.clone(), ShopsItemId::from(x.to_string())),
+            UpdateItemCommand {
+                price: None,
+                state: Some(ItemState::Listed),
+            },
+        )
+    };
+    let cmds = (1..=count).map(mk_entry).collect();
+    let client = get_dynamodb_client().await;
+    let write_res = client.handle_update_items(cmds).await;
+    assert!(write_res.is_err());
+    assert_eq!(count as usize, write_res.unwrap_err().len());
+
+    let actual_count = client
+        .scan()
+        .table_name("items")
+        .send()
+        .await
+        .unwrap()
+        .count;
+    assert_eq!(0, actual_count);
+}
+
+#[rstest::rstest]
+#[case::ten(10)]
+#[case::twentyfive(25)]
+#[case::fortytwo(42)]
+#[case::fifty(50)]
+#[case::sixtynine(69)]
+#[case::onehundred(100)]
+#[case::fourhundredandtwenty(420)]
+#[case::fivehundred(500)]
+#[localstack_test(services = [DynamoDB])]
+async fn should_partially_fail_non_existent_items_for_handle_update_items_with_commands_count(
+    #[case] count: i32,
+) {
+    let shop_id = ShopId::new();
+    let cmd = CreateItemCommand {
+        shop_id: shop_id.clone(),
+        shops_item_id: ShopsItemId::from(count.to_string()),
+        shop_name: "Boop".to_string(),
+        title: Default::default(),
+        description: Default::default(),
+        price: None,
+        state: ItemState::Reserved,
+        url: "https://beep.boop.com/baap".to_string(),
+        images: vec![],
+    };
+
+    // create only the last item
+    let client = get_dynamodb_client().await;
+    let write_res = client.handle_create_items(vec![cmd.clone()]).await;
+    assert!(write_res.is_ok());
+
+    // manually insert the materialized one
+    let materialized = ItemRecord {
+        pk: format!("item#shop_id#{}#shops_item_id#{count}", shop_id.clone()),
+        sk: "item#materialized".to_string(),
+        gsi_1_pk: format!("shop_id#{}", shop_id.clone()),
+        gsi_1_sk: "updated#2007-12-24T18:21Z".to_string(),
+        item_id: Default::default(),
+        event_id: Default::default(),
+        shop_id: shop_id.clone(),
+        shops_item_id: ShopsItemId::from(count.to_string()),
+        shop_name: "".to_string(),
+        title: None,
+        title_de: None,
+        title_en: None,
+        description: None,
+        description_de: None,
+        description_en: None,
+        price: None,
+        state: ItemStateRecord::Reserved,
+        url: "".to_string(),
+        images: vec![],
+        hash: ItemHash::new(&None, &ItemState::Reserved),
+        created: OffsetDateTime::now_utc(),
+        updated: OffsetDateTime::now_utc(),
+    };
+    let write_materialized_output = client
+        .put_item_records(Batch::from([materialized]))
+        .await
+        .unwrap();
+    assert!(
+        write_materialized_output
+            .unprocessed_items
+            .unwrap_or_default()
+            .is_empty()
+    );
+
+    // update all items
+    let mk_entry = |x: i32| {
+        (
+            ItemKey::new(shop_id.clone(), ShopsItemId::from(x.to_string())),
+            UpdateItemCommand {
+                price: None,
+                state: Some(ItemState::Listed),
+            },
+        )
+    };
+    let cmds = (1..=count).map(mk_entry).collect();
+    let client = get_dynamodb_client().await;
+    let write_res = client.handle_update_items(cmds).await;
+    assert!(write_res.is_err());
+    assert_eq!(count as usize - 1, write_res.unwrap_err().len());
+
+    let actual_count = client
+        .scan()
+        .table_name("items")
+        .send()
+        .await
+        .unwrap()
+        .count;
+    assert_eq!(3, actual_count);
 }
