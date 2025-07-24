f2ff64055a8bd3b65b92faa66006eaeadb517335
bruderj15 <julian.bruder@outlook.com>
2025-07-24 17:57:29 +0200

    main: refine trait `Scrape` with neat lifetimes

--- src/scrape/src/scrape-core/src/spec.rs
+++ src/scrape/src/scrape-core/src/spec.rs
@@ -1,8 +1,6 @@
 use crate::data::ScrapeItem;
 use async_stream::try_stream;
 use async_trait::async_trait;
-use common::currency::data::CurrencyData;
-use common::language::data::LanguageData;
 use futures::stream::BoxStream;
 use std::error::Error;
 use std::time::Duration;
@@ -11,8 +9,6 @@ use tracing::info;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub struct ScraperConfig {
     pub page_delay: Option<Duration>,
-    pub language: Option<LanguageData>,
-    pub currency: Option<CurrencyData>,
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -22,20 +18,34 @@ pub enum ScrapeError {
 }
 
 #[async_trait]
-pub trait Scraper: Send + Sync {
+pub trait Scraper<Client: Send + Sync>: Send + Sync {
+    fn shop_id_str(&self) -> &'static str;
+    fn shop_name_str(&self) -> &'static str;
+
     async fn scrape_page(
         &self,
+        client: &Client,
         scraper_config: ScraperConfig,
         page_num: u32,
     ) -> Result<Vec<ScrapeItem>, ScrapeError>;
 
-    fn scrape(&self, scraper_config: ScraperConfig) -> BoxStream<Result<ScrapeItem, ScrapeError>> {
+    fn scrape<'a>(
+        &'a self,
+        client: &'a Client,
+        scraper_config: ScraperConfig,
+    ) -> BoxStream<'a, Result<ScrapeItem, ScrapeError>> {
+        info!(
+            shopId = self.shop_id_str(),
+            shopName = self.shop_name_str(),
+            config = ?scraper_config,
+            "Starting to scrape."
+        );
         Box::pin(try_stream! {
             let mut i: u32 = 1;
             loop {
-                let items = self.scrape_page(scraper_config, i).await?;
+                let items = self.scrape_page(client, scraper_config, i).await?;
                 let items_count = items.len();
-                info!(page = i, total = items_count, "Scraped page.");
+                info!(shopId = self.shop_id_str(), page = i, total = items_count, "Scraped page.");
                 if items_count == 0 {
                     break;
                 }
