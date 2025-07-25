78a4a9a488f3b67ce939c3061c38b906d2a46609
bruderj15 <julian.bruder@outlook.com>
2025-07-25 11:28:14 +0200

    main: test `scrape-core`

--- src/scrape/src/scrape-core/src/spec.rs
+++ src/scrape/src/scrape-core/src/spec.rs
@@ -60,3 +60,95 @@ pub trait Scraper<Client: Send + Sync>: Send + Sync {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::data::ScrapeItem;
+    use crate::spec::{ScrapeError, Scraper, ScraperConfig};
+    use async_trait::async_trait;
+    use futures::StreamExt;
+    use item_core::item_state::data::ItemStateData;
+    use std::time::{Duration, SystemTime};
+
+    struct DummyClient;
+    struct DummyScraper;
+
+    #[async_trait]
+    impl Scraper<DummyClient> for DummyScraper {
+        fn shop_id_str(&self) -> &'static str {
+            "dummy-id"
+        }
+
+        fn shop_name_str(&self) -> &'static str {
+            "dummy-name"
+        }
+
+        async fn scrape_page(
+            &self,
+            _: &DummyClient,
+            _: ScraperConfig,
+            page_num: u32,
+        ) -> Result<Vec<ScrapeItem>, ScrapeError> {
+            let mk_scrape_item = || ScrapeItem {
+                shop_id: Default::default(),
+                shops_item_id: Default::default(),
+                shop_name: "".to_string(),
+                title: Default::default(),
+                description: Default::default(),
+                price: None,
+                state: ItemStateData::Listed,
+                url: "".to_string(),
+                images: vec![],
+            };
+            match page_num {
+                1 => Ok((0..20).map(|_| mk_scrape_item()).collect::<Vec<_>>()),
+                2 => Ok((0..20).map(|_| mk_scrape_item()).collect::<Vec<_>>()),
+                3 => Ok((0..20).map(|_| mk_scrape_item()).collect::<Vec<_>>()),
+                4 => Ok((0..20).map(|_| mk_scrape_item()).collect::<Vec<_>>()),
+                5 => Ok((0..1).map(|_| mk_scrape_item()).collect::<Vec<_>>()),
+                _ => Ok(vec![]),
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn should_scrape_all_pages_until_no_items_returned() {
+        let actual = DummyScraper
+            .scrape(&DummyClient, ScraperConfig::default())
+            .collect::<Vec<Result<ScrapeItem, ScrapeError>>>()
+            .await
+            .into_iter()
+            .filter_map(Result::ok)
+            .collect::<Vec<_>>();
+
+        assert_eq!(81, actual.len());
+    }
+
+    #[rstest::rstest]
+    #[case(50)]
+    #[case(100)]
+    #[case(200)]
+    #[case(1000)]
+    #[case(3000)]
+    #[tokio::test]
+    async fn should_scrape_all_pages_respecting_configured_inter_page_delay(#[case] delay_ms: u64) {
+        let t1 = SystemTime::now();
+        let _ = DummyScraper
+            .scrape(
+                &DummyClient,
+                ScraperConfig {
+                    page_delay: Some(Duration::from_millis(delay_ms)),
+                },
+            )
+            .collect::<Vec<Result<ScrapeItem, ScrapeError>>>()
+            .await
+            .into_iter()
+            .filter_map(Result::ok)
+            .collect::<Vec<_>>();
+        let t2 = SystemTime::now();
+
+        let time_spent = t2.duration_since(t1).unwrap().as_millis();
+        // 5 pages (4 transitions)
+        assert!(time_spent > delay_ms as u128 * 4);
+    }
+}
