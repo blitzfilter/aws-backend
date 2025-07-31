042a013ea3b4998033c931e7f1e5bda656217b57
bruderj15 <julian.bruder@outlook.com>
2025-07-31 13:29:12 +0200

    main: test `From<ItemKey> for String`

--- src/common/src/item_id.rs
+++ src/common/src/item_id.rs
@@ -128,6 +128,27 @@ mod tests {
     #[case::shop_containing_separators("abcdefg#boop#789451#655#665#4", "1874874")]
     #[case::item_containing_separator("abcdefg", "1874874#489746152")]
     #[case::item_containing_separator("abcdefg", "1874874#489746152#49874651#845")]
+    fn should_into_string_item_key(#[case] shop_id: &str, #[case] shops_item_id: &str) {
+        use crate::item_id::ItemKey;
+
+        let expected = format!("shop_id#{shop_id}#shops_item_id#{shops_item_id}");
+
+        let actual: String = ItemKey {
+            shop_id: shop_id.into(),
+            shops_item_id: shops_item_id.into(),
+        }
+        .into();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[rstest::rstest]
+    #[case::differing("abcdefg", "123456")]
+    #[case::identical("abcdefg", "abcdefg")]
+    #[case::shop_containing_separator("abcdefg#boop", "1874874")]
+    #[case::shop_containing_separators("abcdefg#boop#789451#655#665#4", "1874874")]
+    #[case::item_containing_separator("abcdefg", "1874874#489746152")]
+    #[case::item_containing_separator("abcdefg", "1874874#489746152#49874651#845")]
     fn should_parse_item_key(#[case] shop_id: &str, #[case] shops_item_id: &str) {
         use crate::item_id::ItemKey;
 
