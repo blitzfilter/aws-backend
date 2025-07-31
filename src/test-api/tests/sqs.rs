2f87750444a9d1e96df57ab6e14b4d91f8b8bcc7
bruderj15 <julian.bruder@outlook.com>
2025-07-31 16:43:58 +0200

    main: verify by reading SQS

--- src/test-api/tests/sqs.rs
+++ src/test-api/tests/sqs.rs
@@ -7,12 +7,24 @@ async fn should_run_without_errors() {}
 
 #[localstack_test(services = [SQS])]
 async fn should_post_to_sqs() {
-    let _ = get_sqs_client()
-        .await
+    let client = get_sqs_client().await;
+    let _ = client
         .send_message()
         .queue_url(SQS.queue_url())
-        .message_body("{}")
+        .message_body(r#"{"foo":"bar"}"#)
         .send()
         .await
         .unwrap();
+
+    let res = client
+        .receive_message()
+        .queue_url(SQS.queue_url())
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(
+        r#"{"foo":"bar"}"#,
+        res.messages.unwrap().remove(0).body.unwrap()
+    )
 }
