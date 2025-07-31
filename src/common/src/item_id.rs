c24d6478c11b0265450d3d5a27a7b69c82d9615c
bruderj15 <julian.bruder@outlook.com>
2025-07-31 13:18:47 +0200

    main: test item-key parsing

--- src/common/src/item_id.rs
+++ src/common/src/item_id.rs
@@ -97,3 +97,48 @@ impl TryFrom<&str> for ItemId {
         Uuid::parse_str(s).map(Self)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[rstest::rstest]
+    #[case::differing("abcdefg", "123456")]
+    #[case::identical("abcdefg", "abcdefg")]
+    #[case::shop_containing_separator("abcdefg#boop", "1874874")]
+    #[case::shop_containing_separators("abcdefg#boop#789451#655#665#4", "1874874")]
+    #[case::item_containing_separator("abcdefg", "1874874#489746152")]
+    #[case::item_containing_separator("abcdefg", "1874874#489746152#49874651#845")]
+    fn should_display_item_key(#[case] shop_id: &str, #[case] shops_item_id: &str) {
+        use crate::item_id::ItemKey;
+
+        let expected = format!("shop_id#{shop_id}#shops_item_id#{shops_item_id}");
+
+        let actual = ItemKey {
+            shop_id: shop_id.into(),
+            shops_item_id: shops_item_id.into(),
+        }
+        .to_string();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[rstest::rstest]
+    #[case::differing("abcdefg", "123456")]
+    #[case::identical("abcdefg", "abcdefg")]
+    #[case::shop_containing_separator("abcdefg#boop", "1874874")]
+    #[case::shop_containing_separators("abcdefg#boop#789451#655#665#4", "1874874")]
+    #[case::item_containing_separator("abcdefg", "1874874#489746152")]
+    #[case::item_containing_separator("abcdefg", "1874874#489746152#49874651#845")]
+    fn should_parse_item_key(#[case] shop_id: &str, #[case] shops_item_id: &str) {
+        use crate::item_id::ItemKey;
+
+        let payload = format!("shop_id#{shop_id}#shops_item_id#{shops_item_id}");
+        let actual = ItemKey::try_from(payload.as_str());
+
+        let expected = ItemKey {
+            shop_id: shop_id.into(),
+            shops_item_id: shops_item_id.into(),
+        };
+
+        assert_eq!(expected, actual.unwrap());
+    }
+}
