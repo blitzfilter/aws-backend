ad299394fdfbe28c14bea0dccb90d38582bd5c22
bruderj15 <julian.bruder@outlook.com>
2025-08-01 17:32:54 +0200

    main: add additional test-api sqs test

--- src/test-api/tests/sqs.rs
+++ src/test-api/tests/sqs.rs
@@ -28,3 +28,12 @@ async fn should_post_to_sqs() {
         res.messages.unwrap().remove(0).body.unwrap()
     )
 }
+
+#[localstack_test(services = [SQS, Sqs { name: "test_sqs_foo" }])]
+async fn should_create_multiple_sqs() {
+    let client = get_sqs_client().await;
+
+    let list_qs = client.list_queues().max_results(1000).send().await.unwrap();
+
+    assert_eq!(2, list_qs.queue_urls.unwrap().len())
+}
