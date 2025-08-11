01adbffc43285dfb9d8affca0ec7923b4f9b6441
bruderj15 <julian.bruder@outlook.com>
2025-08-11 19:27:21 +0200

    main: implement `item_lambda_materialize_dynamodb_update::handler`

--- src/item/src/item-lambda/src/item-lambda-materialize-dynamodb-update/src/lib.rs
+++ src/item/src/item-lambda/src/item-lambda-materialize-dynamodb-update/src/lib.rs
@@ -1,12 +1,105 @@
-use aws_lambda_events::sqs::{SqsBatchResponse, SqsEvent};
-use aws_sdk_dynamodb::Client;
-use common::has::Has;
-use lambda_runtime::LambdaEvent;
+use std::collections::HashMap;
 
-#[tracing::instrument(skip(_service, event), fields(requestId = %event.context.request_id))]
+use aws_lambda_events::sqs::{BatchItemFailure, SqsBatchResponse, SqsEvent, SqsMessage};
+use common::has::HasKey;
+use common::item_id::ItemKey;
+use item_core::item::update_record::ItemRecordUpdate;
+use item_core::item_event::record::ItemEventRecord;
+use item_write::repository::PersistItemRepository;
+use lambda_runtime::LambdaEvent;
+use tracing::{error, info};
+
+#[tracing::instrument(skip(repository, event), fields(requestId = %event.context.request_id))]
 pub async fn handler(
-    _service: &impl Has<Client>,
+    repository: &impl PersistItemRepository,
     event: LambdaEvent<SqsEvent>,
 ) -> Result<SqsBatchResponse, lambda_runtime::Error> {
-    todo!()
+    let records_count = event.payload.records.len();
+    info!(total = records_count, "Handler invoked.",);
+
+    let mut failed_message_ids = Vec::new();
+    let mut skipped_count = 0;
+    let mut updates = Vec::with_capacity(records_count);
+    let mut message_ids: HashMap<ItemKey, String> = HashMap::with_capacity(records_count);
+
+    for message in event.payload.records {
+        if let Some(update) = extract_message_data(
+            message,
+            &mut failed_message_ids,
+            &mut skipped_count,
+            &mut message_ids,
+        ) {
+            updates.push(update);
+        }
+    }
+
+    for (key, update) in updates {
+        let update_res = repository
+            .update_item_record(&key.shop_id, &key.shops_item_id, update)
+            .await;
+        if let Err(err) = update_res {
+            error!(error = ?err, itemKey = %key, "Failed update.");
+            match message_ids.remove(&key) {
+                Some(message_id) => failed_message_ids.push(message_id),
+                None => {
+                    error!(
+                        itemKey = %key,
+                        "There exists no message_id for failed ItemRecord."
+                    );
+                }
+            }
+        }
+    }
+
+    let failure_count = failed_message_ids.len();
+    info!(
+        successful = records_count - failure_count - skipped_count,
+        failures = failure_count,
+        skipped = skipped_count,
+        "Handler finished.",
+    );
+    let sqs_batch_response = SqsBatchResponse {
+        batch_item_failures: failed_message_ids
+            .into_iter()
+            .map(|item_identifier| BatchItemFailure { item_identifier })
+            .collect(),
+    };
+    Ok(sqs_batch_response)
+}
+
+fn extract_message_data(
+    message: SqsMessage,
+    failed_message_ids: &mut Vec<String>,
+    skipped_count: &mut usize,
+    message_ids: &mut HashMap<ItemKey, String>,
+) -> Option<(ItemKey, ItemRecordUpdate)> {
+    let message_id = message
+        .message_id
+        .expect("shouldn't receive an SQS-Message without 'message_id' because AWS sets it.");
+
+    match message.body {
+        None => {
+            info!("Received empty body. Skipping message.");
+            *skipped_count += 1;
+            None
+        }
+        Some(item_json) => match serde_json::from_str::<ItemEventRecord>(&item_json) {
+            Ok(event_record) => {
+                let key = event_record.key();
+                let update_record = ItemRecordUpdate::from(event_record);
+                message_ids.insert(key.clone(), message_id);
+                Some((key, update_record))
+            }
+            Err(e) => {
+                error!(
+                    error = %e,
+                    type = %std::any::type_name::<ItemEventRecord>(),
+                    payload = %item_json,
+                    "Failed deserializing."
+                );
+                failed_message_ids.push(message_id);
+                None
+            }
+        },
+    }
 }
