ad9811c24f8dfd0afba50204063b98145524a6f5
bruderj15 <julian.bruder@outlook.com>
2025-08-12 18:14:39 +0200

    main: add `impl TryFrom<ItemEventRecord> for ItemDocument` & refine document-types

--- src/item/src/item-core/src/item/document.rs
+++ src/item/src/item-core/src/item/document.rs
@@ -1,10 +1,15 @@
+use crate::item_event::record::ItemEventRecord;
 use crate::item_state::document::ItemStateDocument;
+use common::error::mapping_error::PersistenceMappingError;
+use common::error::missing_field::MissingPersistenceField;
 use common::item_id::{ItemId, ItemKey};
 use common::shop_id::ShopId;
 use common::shops_item_id::ShopsItemId;
 use common::{event_id::EventId, has::HasKey};
+use field::field;
 use serde::{Deserialize, Serialize};
 use time::OffsetDateTime;
+use url::Url;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -53,10 +58,10 @@ pub struct ItemDocument {
 
     pub is_available: bool,
 
-    pub url: String,
+    pub url: Url,
 
     #[serde(skip_serializing_if = "Vec::is_empty", default)]
-    pub images: Vec<String>,
+    pub images: Vec<Url>,
 
     #[serde(with = "time::serde::rfc3339")]
     pub created: OffsetDateTime,
@@ -79,5 +84,44 @@ impl HasKey for ItemDocument {
             shop_id: self.shop_id.clone(),
             shops_item_id: self.shops_item_id.clone(),
         }
+    }
+}
+
+impl TryFrom<ItemEventRecord> for ItemDocument {
+    type Error = PersistenceMappingError;
+
+    fn try_from(event_record: ItemEventRecord) -> Result<Self, Self::Error> {
+        let state = event_record
+            .state
+            .map(ItemStateDocument::from)
+            .ok_or_else(|| MissingPersistenceField::new(field!(state@ItemEventRecord)))?;
+        let document = ItemDocument {
+            item_id: event_record.item_id,
+            event_id: event_record.event_id,
+            shop_id: event_record.shop_id,
+            shops_item_id: event_record.shops_item_id,
+            shop_name: event_record
+                .shop_name
+                .ok_or_else(|| MissingPersistenceField::new(field!(shop_name@ItemEventRecord)))?,
+            title_de: event_record.title_de,
+            title_en: event_record.title_en,
+            description_de: event_record.description_de,
+            description_en: event_record.description_en,
+            price_eur: event_record.price_eur,
+            price_usd: event_record.price_usd,
+            price_gbp: event_record.price_gbp,
+            price_aud: event_record.price_aud,
+            price_cad: event_record.price_cad,
+            price_nzd: event_record.price_nzd,
+            state,
+            url: event_record
+                .url
+                .ok_or_else(|| MissingPersistenceField::new(field!(url@ItemEventRecord)))?,
+            images: event_record.images.unwrap_or_default(),
+            created: event_record.timestamp,
+            updated: event_record.timestamp,
+            is_available: matches!(state, ItemStateDocument::Available),
+        };
+        Ok(document)
     }
 }
