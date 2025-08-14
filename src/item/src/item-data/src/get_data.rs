90367609e32aee78d455a4279771cbca447b582f
bruderj15 <julian.bruder@outlook.com>
2025-08-14 16:10:46 +0200

    main: test serialization of `GetItemData`

--- src/item/src/item-data/src/get_data.rs
+++ src/item/src/item-data/src/get_data.rs
@@ -9,6 +9,7 @@ use common::shops_item_id::ShopsItemId;
 use item_core::item::LocalizedItemView;
 use serde::Serialize;
 use time::OffsetDateTime;
+use url::Url;
 
 #[derive(Debug, Clone, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -33,10 +34,10 @@ pub struct GetItemData {
 
     pub state: ItemStateData,
 
-    pub url: String,
+    pub url: Url,
 
     #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub images: Vec<String>,
+    pub images: Vec<Url>,
 
     #[serde(with = "time::serde::rfc3339")]
     pub created: OffsetDateTime,
@@ -68,10 +69,82 @@ impl From<LocalizedItemView> for GetItemData {
             description: item_view.description.map(LocalizedTextData::from),
             price: item_view.price.map(PriceData::from),
             state: item_view.state.into(),
-            url: item_view.url.into(),
-            images: item_view.images.into_iter().map(String::from).collect(),
+            url: item_view.url,
+            images: item_view.images,
             created: item_view.created,
             updated: item_view.updated,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use common::{
+        currency::data::CurrencyData,
+        event_id::EventId,
+        item_id::ItemId,
+        language::data::{LanguageData, LocalizedTextData},
+        price::data::PriceData,
+        shop_id::ShopId,
+        shops_item_id::ShopsItemId,
+    };
+    use serde_json::json;
+    use time::macros::utc_datetime;
+    use url::Url;
+
+    use crate::{get_data::GetItemData, item_state_data::ItemStateData};
+
+    #[test]
+    fn should_serialize_get_item_data() {
+        let item_id = ItemId::new();
+        let event_id = EventId::new();
+        let shop_id = ShopId::new();
+        let shops_item_id = ShopsItemId::new();
+        let dto = GetItemData {
+            item_id,
+            event_id,
+            shop_id: shop_id.clone(),
+            shops_item_id: shops_item_id.clone(),
+            shop_name: "My shop".into(),
+            title: LocalizedTextData::new("Mein titel", LanguageData::De),
+            description: Some(LocalizedTextData::new("My description", LanguageData::En)),
+            price: Some(PriceData::new(CurrencyData::Eur, 50000)),
+            state: ItemStateData::Reserved,
+            url: Url::parse("https://my-shop.de/item").unwrap(),
+            images: vec![
+                Url::parse("https://my-shop.de/item/images/1").unwrap(),
+                Url::parse("https://my-shop.de/item/images/2").unwrap(),
+            ],
+            created: utc_datetime!(2025 - 05 - 05 0:00).into(),
+            updated: utc_datetime!(2025 - 05 - 05 0:00).into(),
+        };
+
+        let expected = json!({
+            "itemId": item_id,
+            "eventId": event_id,
+            "shopId": shop_id,
+            "shopsItemId": shops_item_id,
+            "shopName": "My shop",
+            "title": {
+                "text": "Mein titel",
+                "language": "de"
+            },
+            "description": {
+                "text": "My description",
+                "language": "en"
+            },
+            "price": {
+                "currency": "EUR",
+                "amount": 50000
+            },
+            "state": "RESERVED",
+            "url": "https://my-shop.de/item",
+            "images": ["https://my-shop.de/item/images/1", "https://my-shop.de/item/images/2"],
+            "created": "2025-05-05T00:00:00Z",
+            "updated": "2025-05-05T00:00:00Z"
+        });
+
+        let actual = serde_json::to_value(dto).unwrap();
+        assert_eq!(expected, actual);
     }
 }
