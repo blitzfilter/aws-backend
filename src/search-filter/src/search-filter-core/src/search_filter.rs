373261757cd9de24ffddbee7e070d374078f45c1
bruderj15 <julian.bruder@outlook.com>
2025-08-17 13:05:18 +0200

    main: price-query is currency-agnostic in domain

--- src/search-filter/src/search-filter-core/src/search_filter.rs
+++ src/search-filter/src/search-filter-core/src/search_filter.rs
@@ -1,19 +1,14 @@
 use crate::{array_query::ArrayQuery, range_query::RangeQuery, text_query::TextQuery};
 use common::item_state::domain::ItemState;
-use common::{currency::domain::Currency, price::domain::MonetaryAmount};
+use common::price::domain::MonetaryAmount;
 use time::OffsetDateTime;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct SearchFilter {
     item_query: TextQuery,
     shop_name_query: Option<TextQuery>,
-    price_query: Option<MonetaryAmount>,
+    price_query: Option<RangeQuery<MonetaryAmount>>,
     state_query: ArrayQuery<ItemState>,
     created_query: Option<RangeQuery<OffsetDateTime>>,
     updated_query: Option<RangeQuery<OffsetDateTime>>,
-}
-
-pub struct PriceQuery {
-    pub range: RangeQuery<MonetaryAmount>,
-    pub currency: Currency,
 }
