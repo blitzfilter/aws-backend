da67d098f71ea0de992dad1c7104f43719cb78df
bruderj15 <julian.bruder@outlook.com>
2025-08-18 20:58:19 +0200

    main: implement `ItemOpenSearchRepository::search_item_documents` & add simple test

--- src/item/src/item-opensearch/src/repository.rs
+++ src/item/src/item-opensearch/src/repository.rs
@@ -1,11 +1,22 @@
-use common::opensearch::bulk_response::BulkResponse;
 use crate::item_document::ItemDocument;
+use crate::item_state_document::ItemStateDocument;
 use crate::item_update_document::ItemUpdateDocument;
 use async_trait::async_trait;
+use common::currency::domain::Currency;
 use common::item_id::ItemId;
-use opensearch::{BulkOperation, BulkOperations, BulkParts};
+use common::item_state::domain::ItemState;
+use common::language::domain::Language;
+use common::opensearch::{bulk_response::BulkResponse, search_response::SearchResponse};
+use common::page::Page;
+use common::sort::{Sort, SortDirection};
+use item_core::item::SortItemField;
+use opensearch::{BulkOperation, BulkOperations, BulkParts, SearchParts};
+use search_filter_core::search_filter::SearchFilter;
+use serde::ser::Error;
 use serde_json::json;
 use std::collections::HashMap;
+use std::ops::Deref;
+use time::format_description::well_known;
 
 #[async_trait]
 #[mockall::automock]
@@ -19,6 +30,15 @@ pub trait ItemOpenSearchRepository {
         &self,
         updates: HashMap<ItemId, ItemUpdateDocument>,
     ) -> Result<BulkResponse, opensearch::Error>;
+
+    async fn search_item_documents(
+        &self,
+        search_filter: &SearchFilter,
+        language: &Language,
+        currency: &Currency,
+        sort: &Option<Sort<SortItemField>>,
+        page: &Option<Page>,
+    ) -> Result<SearchResponse<ItemDocument>, opensearch::Error>;
 }
 
 pub struct ItemOpenSearchRepositoryImpl<'a> {
@@ -73,5 +93,178 @@ impl<'a> ItemOpenSearchRepository for ItemOpenSearchRepositoryImpl<'a> {
             .await?
             .json::<BulkResponse>()
             .await
+    }
+
+    async fn search_item_documents(
+        &self,
+        search_filter: &SearchFilter,
+        language: &Language,
+        currency: &Currency,
+        sort: &Option<Sort<SortItemField>>,
+        page: &Option<Page>,
+    ) -> Result<SearchResponse<ItemDocument>, opensearch::Error> {
+        let mut must = vec![];
+
+        let (title_field, description_field) = match language {
+            Language::De => ("titleDe", "descriptionDe"),
+            Language::En => ("titleEn", "descriptionEn"),
+            _ => ("titleDe", "descriptionDe"),
+        };
+        must.push(json!({
+            "multi_match": {
+                "query": search_filter.item_query.as_ref(),
+                "fields": [
+                    format!("{title_field}^3"),
+                    format!("{description_field}^1"),
+                ],
+                "fuzziness": "AUTO",
+                "operator": "and"
+            }
+        }));
+
+        if let Some(shop_name_query) = &search_filter.shop_name_query {
+            must.push(json!({
+                "match": {
+                    "shopName": {
+                        "query": shop_name_query.deref(),
+                        "fuzziness": "AUTO",
+                        "operator": "and"
+                    }
+                }
+            }));
+        }
+
+        match &*search_filter.state_query {
+            [] => {}
+            [ItemState::Available] => {
+                must.push(json!({
+                    "term": { "isAvailable": true }
+                }));
+            }
+            states => {
+                let state_values: Vec<&str> = states
+                    .iter()
+                    .copied()
+                    .map(ItemStateDocument::from)
+                    .map(|s| s.as_str())
+                    .collect();
+
+                must.push(json!({
+                    "terms": { "state": state_values }
+                }));
+            }
+        }
+
+        let price_field = match currency {
+            Currency::Eur => "price_eur",
+            Currency::Gbp => "price_gbp",
+            Currency::Usd => "price_usd",
+            Currency::Aud => "price_aud",
+            Currency::Cad => "price_cad",
+            Currency::Nzd => "price_nzd",
+        };
+        if let Some(min) = search_filter
+            .price_query
+            .and_then(|price_query| price_query.min)
+        {
+            must.push(json!({
+                "range": { price_field: { "gte": min.deref() } }
+            }));
+        }
+        if let Some(max) = search_filter
+            .price_query
+            .and_then(|price_query| price_query.max)
+        {
+            must.push(json!({
+                "range": { price_field: { "lte": max.deref() } }
+            }));
+        }
+
+        if let Some(min) = search_filter
+            .created_query
+            .and_then(|created_query| created_query.min)
+        {
+            let formatted_min = min
+                .format(&well_known::Rfc3339)
+                .map_err(serde_json::Error::custom)?;
+            must.push(json!({
+                "range": { "created": { "gte": formatted_min } }
+            }));
+        }
+        if let Some(max) = search_filter
+            .created_query
+            .and_then(|created_query| created_query.max)
+        {
+            let formatted_max = max
+                .format(&well_known::Rfc3339)
+                .map_err(serde_json::Error::custom)?;
+            must.push(json!({
+                "range": { "created": { "lte": formatted_max } }
+            }));
+        }
+
+        if let Some(min) = search_filter
+            .updated_query
+            .and_then(|updated_query| updated_query.min)
+        {
+            let formatted_min = min
+                .format(&well_known::Rfc3339)
+                .map_err(serde_json::Error::custom)?;
+            must.push(json!({
+                "range": { "updated": { "gte": formatted_min } }
+            }));
+        }
+        if let Some(max) = search_filter
+            .updated_query
+            .and_then(|updated_query| updated_query.max)
+        {
+            let formatted_max = max
+                .format(&well_known::Rfc3339)
+                .map_err(serde_json::Error::custom)?;
+            must.push(json!({
+                "range": { "updated": { "lte": formatted_max } }
+            }));
+        }
+
+        let mut body = json!({
+            "query": {
+                "bool": { "must": must }
+            }
+        });
+
+        if let Some(p) = page {
+            body.as_object_mut()
+                .unwrap()
+                .insert("from".to_string(), json!(p.from));
+            body.as_object_mut()
+                .unwrap()
+                .insert("size".to_string(), json!(p.size));
+        }
+
+        if let Some(sort) = sort {
+            let sort_field = match sort.field {
+                SortItemField::Price => price_field,
+                SortItemField::Created => "created",
+                SortItemField::Updated => "updated",
+            };
+            let order = match sort.direction {
+                SortDirection::Asc => "asc",
+                SortDirection::Desc => "desc",
+            };
+            body.as_object_mut().unwrap().insert(
+                "sort".to_string(),
+                json!([{ sort_field: { "order": order } }]),
+            );
+        }
+
+        let response = self
+            .client
+            .search(SearchParts::Index(&["items"]))
+            .body(body)
+            .send()
+            .await?;
+        let search_response = response.json::<SearchResponse<ItemDocument>>().await?;
+
+        Ok(search_response)
     }
 }
--- src/item/src/item-opensearch/tests/repository.rs
+++ src/item/src/item-opensearch/tests/repository.rs
@@ -1,12 +1,17 @@
+use common::currency::domain::Currency;
 use common::event_id::EventId;
 use common::item_id::ItemId;
+use common::language::domain::Language;
 use common::shops_item_id::ShopsItemId;
 use item_opensearch::item_document::ItemDocument;
 use item_opensearch::item_state_document::ItemStateDocument;
 use item_opensearch::item_update_document::ItemUpdateDocument;
 use item_opensearch::repository::{ItemOpenSearchRepository, ItemOpenSearchRepositoryImpl};
 use opensearch::http::Url;
+use search_filter_core::array_query::ArrayQuery;
+use search_filter_core::search_filter::SearchFilter;
 use std::collections::HashMap;
+use std::time::Duration;
 use test_api::*;
 use time::OffsetDateTime;
 
@@ -179,4 +184,56 @@ async fn should_update_item_document() {
     let actual = read_by_id("items", item_id).await;
 
     assert_eq!(expected, actual);
+}
+
+#[localstack_test(services = [OpenSearch()])]
+async fn should_search_item_documents() {
+    let expected = ItemDocument {
+        item_id: Default::default(),
+        event_id: Default::default(),
+        shop_id: Default::default(),
+        shops_item_id: ShopsItemId::from("abcdefgh"),
+        shop_name: "Foo".to_string(),
+        title_de: Some("Hallo Welt".to_string()),
+        title_en: Some("Baz".to_string()),
+        description_de: Some("Lorem ipsum dolor sit amet".to_string()),
+        description_en: Some("Lorem ipsum dolor sit amet".to_string()),
+        price_eur: Some(99),
+        price_usd: None,
+        price_gbp: None,
+        price_aud: None,
+        price_cad: None,
+        price_nzd: None,
+        state: ItemStateDocument::Available,
+        is_available: false,
+        url: Url::parse("https://foo.com/bar").unwrap(),
+        images: vec![Url::parse("https://foo.com/bar").unwrap()],
+        created: OffsetDateTime::now_utc(),
+        updated: OffsetDateTime::now_utc(),
+    };
+    let client = get_opensearch_client().await;
+    let repository = ItemOpenSearchRepositoryImpl::new(client);
+    let response = repository
+        .create_item_documents(vec![expected.clone()])
+        .await
+        .unwrap();
+    assert!(!response.errors);
+    refresh_index("items").await;
+
+    tokio::time::sleep(Duration::from_millis(3000)).await;
+
+    let search_filter = SearchFilter {
+        item_query: "Hallo Welt".into(),
+        shop_name_query: None,
+        price_query: None,
+        state_query: ArrayQuery(vec![]),
+        created_query: None,
+        updated_query: None,
+    };
+    let response = repository
+        .search_item_documents(&search_filter, &Language::De, &Currency::Eur, &None, &None)
+        .await
+        .unwrap();
+
+    println!("{:?}", response.hits);
 }
