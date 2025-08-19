b8c5de5f69a885ef7ef01ff25ecdc95eab267997
bruderj15 <julian.bruder@outlook.com>
2025-08-19 18:51:15 +0200

    main: refine crate `item-service` with features

--- src/item/src/item-service/src/lib.rs
+++ src/item/src/item-service/src/lib.rs
@@ -1,6 +1,11 @@
+#[cfg(feature = "dynamodb")]
 pub mod command_service;
+
+#[cfg(feature = "dynamodb")]
 pub mod get_service;
 pub mod item_command;
 pub mod item_command_data;
 pub mod item_state_command_data;
+
+#[cfg(feature = "opensearch")]
 pub mod query_service;
