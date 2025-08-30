3d959d3680db54975d294ffad2966e490e6f68a8
bruderj15 <julian.bruder@outlook.com>
2025-08-30 09:27:50 +0200

    feat#34: simplify test

--- src/staging-tests/tests/ingest_item_flow.rs
+++ src/staging-tests/tests/ingest_item_flow.rs
@@ -1,41 +1,8 @@
-use aws_sdk_dynamodb::{Client, types::AttributeValue};
 use fake::{Fake, Faker};
-use item_dynamodb::repository::{ItemDynamoDbRepository, ItemDynamoDbRepositoryImpl, mk_pk};
+use item_dynamodb::repository::{ItemDynamoDbRepository, ItemDynamoDbRepositoryImpl};
 use item_service::item_command_data::CreateItemCommandData;
 use staging_tests::{get_cfn_output, get_dynamodb_client, get_sqs_client, staging_test};
 use std::time::{Duration, Instant};
-
-pub async fn wait_for_ddb_partition_to_exist(
-    client: &Client,
-    table: &str,
-    key_name: &str,
-    key_value: &str,
-    timeout_secs: u64,
-) -> Result<(), aws_sdk_dynamodb::Error> {
-    let deadline = Instant::now() + Duration::from_secs(timeout_secs);
-
-    loop {
-        let resp = client
-            .get_item()
-            .table_name(table)
-            .key(key_name, AttributeValue::S(key_value.to_string()))
-            .consistent_read(true) // strong read
-            .send()
-            .await?;
-
-        if resp.item().is_some() {
-            return Ok(());
-        }
-
-        if Instant::now() >= deadline {
-            panic!(
-                "Timeout: partition with key '{key_value}' not found in DynamoDB after '{timeout_secs}' seconds"
-            );
-        }
-
-        tokio::time::sleep(Duration::from_secs(5)).await;
-    }
-}
 
 #[staging_test]
 async fn should_materialize_item_in_dynamodb_for_create_item_command() {
@@ -52,24 +19,29 @@ async fn should_materialize_item_in_dynamodb_for_create_item_command() {
         .unwrap();
 
     let dynamodb_client = get_dynamodb_client().await;
-    wait_for_ddb_partition_to_exist(
-        dynamodb_client,
-        &stack.dynamodb_table_1_name,
-        "pk",
-        &mk_pk(&cmd.shop_id, &cmd.shops_item_id),
-        60,
-    )
-    .await
-    .unwrap();
-
     let repository = ItemDynamoDbRepositoryImpl::new(dynamodb_client, &stack.dynamodb_table_1_name);
-    let materialized = repository
-        .get_item_record(&cmd.shop_id, &cmd.shops_item_id)
-        .await
-        .unwrap()
-        .unwrap();
 
-    assert_eq!(cmd.shop_id, materialized.shop_id);
-    assert_eq!(cmd.shops_item_id, materialized.shops_item_id);
-    assert_eq!(cmd.url, materialized.url);
+    let deadline = Instant::now() + Duration::from_secs(60);
+    loop {
+        let materialized = repository
+            .get_item_record(&cmd.shop_id, &cmd.shops_item_id)
+            .await
+            .unwrap();
+
+        if let Some(materialized) = materialized {
+            assert_eq!(cmd.shop_id, materialized.shop_id);
+            assert_eq!(cmd.shops_item_id, materialized.shops_item_id);
+            assert_eq!(cmd.url, materialized.url);
+            break;
+        }
+
+        if Instant::now() >= deadline {
+            panic!(
+                "Timeout: ItemRecord with shop_id '{}' and shops_item_id '{}' not found in DynamoDB after 60 seconds",
+                cmd.shop_id, cmd.shops_item_id
+            );
+        }
+
+        tokio::time::sleep(Duration::from_secs(5)).await;
+    }
 }
