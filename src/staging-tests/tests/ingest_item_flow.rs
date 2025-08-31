7d22400883052a51ed5bb482cd0fb1a303f92db5
bruderj15 <julian.bruder@outlook.com>
2025-08-31 17:44:12 +0200

    feat#34: add opensearch materialization tests

--- src/staging-tests/tests/ingest_item_flow.rs
+++ src/staging-tests/tests/ingest_item_flow.rs
@@ -1,13 +1,46 @@
-use common::item_state::domain::ItemState;
+use common::{item_state::domain::ItemState, language::data::LocalizedTextData};
 use fake::{Fake, Faker};
 use item_dynamodb::{
     item_record::ItemRecord,
     item_state_record::ItemStateRecord,
     repository::{ItemDynamoDbRepository, ItemDynamoDbRepositoryImpl},
 };
+use item_opensearch::{
+    item_document::ItemDocument,
+    item_state_document::ItemStateDocument,
+    repository::{ItemOpenSearchRepository, ItemOpenSearchRepositoryImpl},
+};
 use item_service::item_command_data::{CreateItemCommandData, UpdateItemCommandData};
-use staging_tests::{get_cfn_output, get_dynamodb_client, get_sqs_client, staging_test};
+use opensearch::{GetParts, IndexParts, params::Refresh};
+use search_filter_core::search_filter::SearchFilter;
+use serde::de::DeserializeOwned;
+use staging_tests::{
+    get_cfn_output, get_dynamodb_client, get_opensearch_client, get_sqs_client, staging_test,
+};
 use std::time::{Duration, Instant};
+
+pub async fn read_by_id<T: DeserializeOwned>(index: &str, id: impl Into<String>) -> T {
+    let get_response = get_opensearch_client()
+        .await
+        .get(GetParts::IndexId(index, &id.into()))
+        .send()
+        .await
+        .unwrap();
+    assert!(get_response.status_code().is_success());
+
+    let response_doc: serde_json::Value = get_response.json().await.unwrap();
+    serde_json::from_value(response_doc["_source"].clone()).unwrap()
+}
+
+pub async fn refresh_index(index: &str) {
+    get_opensearch_client()
+        .await
+        .index(IndexParts::Index(index))
+        .refresh(Refresh::True)
+        .send()
+        .await
+        .unwrap();
+}
 
 #[staging_test]
 async fn should_materialize_item_in_dynamodb_for_create_item_command() {
@@ -109,6 +142,155 @@ async fn should_materialize_item_in_dynamodb_for_update_item_command() {
             panic!(
                 "Timeout: ItemRecord with shop_id '{}' and shops_item_id '{}' \
                     has not been updated in DynamoDB or been updated with expected state after 60 seconds",
+                cmd.shop_id, cmd.shops_item_id
+            );
+        }
+
+        tokio::time::sleep(Duration::from_secs(5)).await;
+    }
+}
+
+#[staging_test]
+async fn should_materialize_item_in_opensearch_for_create_item_command() {
+    let stack = get_cfn_output();
+    let sqs_client = get_sqs_client().await;
+    let mut cmd: CreateItemCommandData = Faker.fake();
+    cmd.native_title = LocalizedTextData {
+        text: "Exactly the expected title".to_string(),
+        language: common::language::data::LanguageData::En,
+    };
+
+    let _ = sqs_client
+        .send_message()
+        .queue_url(stack.item_write_new_queue_url.clone())
+        .message_body(serde_json::to_string(&cmd).unwrap())
+        .send()
+        .await
+        .unwrap();
+
+    let opensearch_client = get_opensearch_client().await;
+    let repository = ItemOpenSearchRepositoryImpl::new(opensearch_client);
+    refresh_index("items").await;
+
+    let deadline = Instant::now() + Duration::from_secs(60);
+    loop {
+        let materialized = repository
+            .search_item_documents(
+                &SearchFilter {
+                    item_query: "Exactly the expected title".try_into().unwrap(),
+                    shop_name_query: None,
+                    price_query: None,
+                    state_query: Default::default(),
+                    created_query: None,
+                    updated_query: None,
+                },
+                &common::language::domain::Language::En,
+                &common::currency::domain::Currency::Eur,
+                &None,
+                &None,
+            )
+            .await
+            .unwrap()
+            .hits
+            .hits
+            .first()
+            .cloned();
+
+        if let Some(materialized) = materialized {
+            assert_eq!(cmd.shop_id, materialized.source.shop_id);
+            assert_eq!(cmd.shops_item_id, materialized.source.shops_item_id);
+            assert_eq!(cmd.url, materialized.source.url);
+            break;
+        }
+
+        if Instant::now() >= deadline {
+            panic!(
+                "Timeout: ItemRecord with shop_id '{}' and shops_item_id '{}' not found in DynamoDB after 60 seconds",
+                cmd.shop_id, cmd.shops_item_id
+            );
+        }
+
+        tokio::time::sleep(Duration::from_secs(5)).await;
+    }
+}
+
+#[staging_test]
+async fn should_materialize_item_in_opensearch_for_update_item_command() {
+    let stack = get_cfn_output();
+    let opensearch_client = get_opensearch_client().await;
+    let repository = ItemOpenSearchRepositoryImpl::new(opensearch_client);
+    let mut materialized_old: ItemDocument = Faker.fake();
+    materialized_old.title_en = Some("Exactly the expected title".to_string());
+    let insert_res = repository
+        .create_item_documents(vec![materialized_old.clone()])
+        .await
+        .unwrap();
+    assert!(!insert_res.errors);
+    refresh_index("items").await;
+
+    let sqs_client = get_sqs_client().await;
+    let new_state = match materialized_old.state {
+        ItemStateDocument::Available => {
+            item_service::item_state_command_data::ItemStateCommandData::Sold
+        }
+        _ => item_service::item_state_command_data::ItemStateCommandData::Available,
+    };
+    let cmd = UpdateItemCommandData {
+        shop_id: materialized_old.shop_id,
+        shops_item_id: materialized_old.shops_item_id,
+        price: None,
+        state: Some(new_state),
+    };
+
+    let _ = sqs_client
+        .send_message()
+        .queue_url(stack.item_write_update_queue_url.clone())
+        .message_body(serde_json::to_string(&cmd).unwrap())
+        .send()
+        .await
+        .unwrap();
+
+    let deadline = Instant::now() + Duration::from_secs(60);
+    loop {
+        refresh_index("items").await;
+        let materialized = repository
+            .search_item_documents(
+                &SearchFilter {
+                    item_query: "Exactly the expected title".try_into().unwrap(),
+                    shop_name_query: None,
+                    price_query: None,
+                    state_query: Default::default(),
+                    created_query: None,
+                    updated_query: None,
+                },
+                &common::language::domain::Language::En,
+                &common::currency::domain::Currency::Usd,
+                &None,
+                &None,
+            )
+            .await
+            .unwrap()
+            .hits
+            .hits
+            .first()
+            .cloned();
+
+        if let Some(materialized) = materialized
+            && ItemState::from(new_state) == ItemState::from(materialized.source.state)
+        {
+            assert_eq!(cmd.shop_id, materialized.source.shop_id);
+            assert_eq!(cmd.shops_item_id, materialized.source.shops_item_id);
+            assert_eq!(
+                ItemState::from(new_state),
+                ItemState::from(materialized.source.state)
+            );
+            break;
+        }
+
+        if Instant::now() >= deadline {
+            panic!(
+                "Timeout: ItemRecord with shop_id '{}' and shops_item_id '{}' \
+                    has not been updated in OpenSearch or been updated with expected state after 60 seconds",
                 cmd.shop_id, cmd.shops_item_id
             );
         }
